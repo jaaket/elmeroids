65a641e505ea71e5c9846b14f0285f4e774d9cce
Jaakko Ketola <jaakko.ketola@aalto.fi>
2015-06-08 20:52:29 +0300

    Game over if you hit an asteroid

--- elmeroids.elm
+++ elmeroids.elm
@@ -8,31 +8,42 @@ import Graphics.Element exposing (..)
 import Time exposing (..)
 import Signal exposing (..)
 import Random exposing (..)
+import Text exposing (fromString)
 
 type alias Vec2 = (Float, Float)
 type alias Object a = { a | pos:Vec2, velocity:Vec2, acceleration:Vec2,
-                        angle:Float, angularVelocity: Float }
+                            angle:Float, angularVelocity: Float,
+                            collisionRadius:Float }
 type alias Input = { up:Bool, down:Bool, left:Bool, right:Bool }
+
+type GameState = Active | Over
 
 type alias Ship = Object {}
 type alias Asteroid = Object {}
-type alias Game = { ship:Ship, asteroids:List Asteroid, windowSize:(Int, Int), seed:Seed }
+type alias Game = { ship:Ship,
+                    asteroids:List Asteroid,
+                    windowSize:(Int, Int),
+                    seed:Seed,
+                    state:GameState }
 
 speed = 0.001
 angularSpeed = 0.005
 
 initShip : Ship
 initShip = { pos=(0,0), velocity=(0,0), acceleration=(0,0),
-             angle=0, angularVelocity=0 }
+             angle=0, angularVelocity=0, collisionRadius=10 }
 
 iterate : Int -> (a -> a) -> a -> a
 iterate n f x = if | n > 0     -> f (iterate (n - 1) f x)
                    | otherwise -> x
 
 initGame : Game
-initGame = iterate 10
-                   addRandomAsteroid
-                   { ship=initShip, asteroids=[], windowSize=(1000, 1000), seed=initialSeed 0 }
+initGame =
+  iterate 10 addRandomAsteroid { ship=initShip,
+                                 asteroids=[],
+                                 windowSize=(1000, 1000),
+                                 seed=initialSeed 0,
+                                 state=Active }
 
 addRandomAsteroid : Game -> Game
 addRandomAsteroid game =
@@ -45,7 +56,8 @@ addRandomAsteroid game =
       (angle, seed'') = generate (float 0 360) seed'
       (angularVel, seed''') = generate (float -0.01 0.01) seed''
   in  { game | asteroids <- { pos=pos, velocity=vel, acceleration=(0,0),
-                              angle=angle, angularVelocity=angularVel } :: game.asteroids,
+                              angle=angle, angularVelocity=angularVel,
+                              collisionRadius=20 } :: game.asteroids,
                seed <- seed''' }
 
 triangle : Form
@@ -97,11 +109,22 @@ updateShip windowSize dt input = maneuver input >> simulate dt
 updateAsteroid : (Int, Int) -> Time -> Asteroid -> Asteroid
 updateAsteroid windowSize dt = simulate dt >> wrapAround windowSize
 
+dist : Vec2 -> Vec2 -> Float
+dist (x1, y1) (x2, y2) = sqrt ((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2))
+
+collides : Object a -> Object b -> Bool
+collides obj1 obj2 = dist obj1.pos obj2.pos < obj1.collisionRadius + obj2.collisionRadius
+
 updateGame : ((Int, Int), Time, Input) -> Game -> Game
 updateGame (windowSize, dt, input) game =
-  { game | ship <- updateShip windowSize dt input game.ship,
-           asteroids <- List.map (updateAsteroid windowSize dt) game.asteroids
-  }
+  case game.state of
+    Active -> { game | ship <- updateShip windowSize dt input game.ship,
+                       asteroids <- List.map (updateAsteroid windowSize dt) game.asteroids,
+                       state <- if List.any (collides game.ship) game.asteroids
+                                  then Over
+                                  else Active
+      }
+    Over -> game
 
 gameState : Signal Game
 gameState = foldp updateGame
@@ -115,11 +138,25 @@ keyInput = map (\keys -> { up = keys.y == 1,
                            right = keys.x == 1 })
                Keyboard.arrows
 
+renderObject : Form -> Object a -> Form
+renderObject form obj = move obj.pos (rotate obj.angle form)
+
 render : (Int, Int) -> Game -> Element
-render windowSize game =
-  let ship = move game.ship.pos (rotate game.ship.angle triangle)
-      asteroids = List.map (\ast -> move ast.pos (rotate ast.angle asteroid)) game.asteroids
-  in  uncurry collage windowSize ([ ship ] ++ asteroids)
+render (w, h) game =
+  let ship = renderObject triangle game.ship
+      asteroids = List.map (renderObject asteroid) game.asteroids
+      world = collage w h <| ship :: asteroids
+      overlay = fromString "Game over!"
+                  |> Text.typeface ["lato", "sans-serif"]
+                  |> Text.color Color.white
+                  |> Text.height 40
+                  |> centered
+                  |> container w h middle
+                  |> color Color.black
+                  |> opacity 0.9
+  in  case game.state of
+           Active -> world
+           Over   -> layers [ world, overlay ]
 
 main : Signal Element
 main = map2 render Window.dimensions gameState
